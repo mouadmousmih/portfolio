f063f71e7013d9e61cec60b114a38ef550f9382a
mouadmousmih <mouadmousmihgmail.com>
2025-03-19 17:02:17 +0100

    setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Section_hero from "@/components/Section_hero";
+
+
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justifiy-center iteams-center  overflow-auto mx-auto 
+    sm:-10px px-5 ">
+      <div className="max-w-7xl w-full">
+        <Section_hero/>
+      </div>
+    </main>
+  );
+}
+
---
+++ components/Section_hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section_hero = () => {
+  return (
+    <div className=' pb-20 pt-36'>
+      mouad 
+    </div>
+  )
+}
+
+export default Section_hero
