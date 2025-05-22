a8822a0f58008bc8b9de169573ae73d693df3b70
mouadmousmih <mouadmousmihgmail.com>
2025-05-22 20:34:41 +0200

    fix: correction de contenu de section contact

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -18,7 +18,7 @@ export default function ContactSection() {
           {/* Contenu au-dessus */}
           <div className="relative z-10">
             <h2 className="text-4xl font-bold text-[#3365ef] font-anta md:animate-glow mb-4">
-              Parlons de ton prochain projet
+              Parlons de Votre prochain projet
             </h2>
             <p className="text-gray-400 mb-8 font-anta">
               Vous avez une idée ou vous cherchez un alternant motivé en DevOps / Web ? <br />
