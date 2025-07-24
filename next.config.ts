d9e905de4944a8f57fe3436764d8cbb7ad99b9d2
mouadmousmih <mouadmousmihgmail.com>
2025-07-24 16:00:39 +0200

    add-> certification lien:docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/docker_certification",
+        destination:
+          "https://www.linkedin.com/learning/certificates/8013aa11ef502a118f9985defe0cb5294b1090f78ba3d99c8b24e3fc9755ef6f?trk=share_certificate ",
+        permanent: false, 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
